5a4d83302d940680b60e0a4e7432378bb781b4f5
System status <slantic207@gmail.com>
2022-08-28 12:34:14 +0200

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+import psutil
 
 
 class ListSystemStatus(MycroftSkill):
@@ -7,8 +8,8 @@ class ListSystemStatus(MycroftSkill):
 
     @intent_file_handler('status.system.list.intent')
     def handle_status_system_list(self, message):
-        cputemp = ''
-        mem = ''
+        cputemp = round(psutil.sensors_temperatures()['cpu_thermal'][0][1], 1)
+        mem = psutil.virtual_memory()[2]
 
         self.speak_dialog('status.system.list', data={
             'mem': mem,
