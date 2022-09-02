60bea4f061df9862334ec3088d87a70a2ede8bec
Slantic <pascalvannugteren@gmail.com>
2022-09-02 19:53:35 +0200

    Changed dialog

--- __init__.py
+++ __init__.py
@@ -10,10 +10,14 @@ class ListSystemStatus(MycroftSkill):
     def handle_status_system_list(self, message):
         cputemp = round(psutil.sensors_temperatures()['cpu_thermal'][0][1], 1)
         mem = psutil.virtual_memory()[2]
+        disk_percentage = psutil.disk_usage('/')[3]
+        disk_free = round(((psutil.disk_usage('/')[2] >> 20) / 1000), 1)
 
         self.speak_dialog('status.system.list', data={
             'mem': mem,
-            'cputemp': cputemp
+            'cputemp': cputemp,
+            'disk_percentage': disk_percentage,
+            'disk_free': disk_free
         })
 
 
